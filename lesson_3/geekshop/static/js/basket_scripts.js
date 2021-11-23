6af28f277da8c517fdc12f0eb163335e97a70832
Mike <smyllik4@bk.ru>
2021-11-24 02:59:02 +0300

    add empty lesson_3

---
+++ lesson_3/geekshop/static/js/basket_scripts.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    $('.basket_list').on('click', 'input[type="number"]', function(){
+        var target_href = event.target;
+        $.ajax({
+            url: "/basket/edit/" + target_href.name + "/" + target_href.value + "/",
+            success: function (data) {
+                $('.basket_list').html(data.result);
+            },
+        });
+        return false;
+    });
+};
