8886edcf4a66b8f33a42d43554ae4f6295290f65
Mike <smyllik4@bk.ru>
2021-12-13 09:01:57 +0300

    add change settings lesson_6.11, add language ru

--- lesson_6/geekshop/geekshop/settings.py
+++ lesson_6/geekshop/geekshop/settings.py
@@ -155,7 +155,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+
 
 TIME_ZONE = 'Europe/Moscow'
 
